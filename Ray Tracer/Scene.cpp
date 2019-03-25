0be103c0f769c15947841354cda0a79abab02fb4
malgorzataMielczarek <45160940+malgorzataMielczarek@users.noreply.github.com>
2019-03-25 19:39:39 +0100

    Structural changes in CScene class

--- Ray Tracer/Scene.cpp
+++ Ray Tracer/Scene.cpp
@@ -28,54 +28,89 @@ bool CScene::parse(std::string fname)
 		iss >> type;
 		if (type.compare("cam_width") == 0)
 		{
-			int cam_width;
-			iss >> cam_width;
-			this->cam.mWidth = cam_width;
+			loadCamWidth(iss);
 		}
 		else if (type.compare("cam_height") == 0)
 		{
-			int cam_height;
-			iss >> cam_height;
-			this->cam.mHeight = cam_height;
+			loadCamHeight(iss);
 		}
 		else if (type.compare("cam_FOV") == 0)
 		{
-			float cam_FOV;
-			iss >> cam_FOV;
-			this->cam.fov = cam_FOV;
+			loadCamFOV(iss);
 		}
 		else if (type.compare("cam_eyep") == 0)
 		{
-			float x, y, z;
-			iss >> x >> y >> z;
-			this->cam.pos = glm::vec3(x, y, z);
+			loadCamPos(iss);
 		}
 		else if (type.compare("cam_lookp") == 0)
 		{
-			float x, y, z;
-			iss >> x >> y >> z;
-			this->cam.target = glm::vec3(x, y, z);
+			loadCamTarget(iss);
 		}
 		else if (type.compare("cam_up") == 0)
 		{
-			float x, y, z;
-			iss >> x >> y >> z;
-			this->cam.up = glm::vec3(x, y, z);
+			loadCamUp(iss);
 		}
 		else if (type.compare("sphere") == 0)
 		{
-			float r, o_x, o_y, o_z, reflect, amb_x, amb_y, amb_z, diff_x, diff_y, diff_z, spec_x, spec_y, spec_z, shininess;
-			iss >> r >> o_x >> o_y >> o_z >> reflect >> amb_x >> amb_y >> amb_z >> diff_x >> diff_y >> diff_z >> spec_x >> spec_y >> spec_z >> shininess;
-			CSphere *sphere = new CSphere;
-			sphere->r = r;
-			sphere->o = glm::vec3(o_x, o_y, o_z);
-			sphere->reflect = reflect;
-			sphere->ambient = glm::vec3(amb_x, amb_y, amb_z);
-			sphere->diffuse = glm::vec3(diff_x, diff_y, diff_z);
-			sphere->specular = glm::vec3(spec_x, spec_y, spec_z);
-			sphere->shininess = shininess;
-			this->mObjects.push_back(sphere);
+			loadSphere(iss);
 		}
 	}
 	return true;
+}
+
+void CScene::loadCamWidth(std::istringstream &iss)
+{
+	int cam_width;
+	iss >> cam_width;
+	this->cam.mWidth = cam_width;
+}
+
+void CScene::loadCamHeight(std::istringstream &iss)
+{
+	int cam_height;
+	iss >> cam_height;
+	this->cam.mHeight = cam_height;
+}
+
+void CScene::loadCamFOV(std::istringstream &iss)
+{
+	float cam_FOV;
+	iss >> cam_FOV;
+	this->cam.fov = cam_FOV;
+}
+
+void CScene::loadCamPos(std::istringstream &iss)
+{
+	float x, y, z;
+	iss >> x >> y >> z;
+	this->cam.pos = glm::vec3(x, y, z);
+}
+
+void CScene::loadCamTarget(std::istringstream &iss)
+{
+	float x, y, z;
+	iss >> x >> y >> z;
+	this->cam.target = glm::vec3(x, y, z);
+}
+
+void CScene::loadCamUp(std::istringstream &iss)
+{
+	float x, y, z;
+	iss >> x >> y >> z;
+	this->cam.up = glm::vec3(x, y, z);
+}
+
+void CScene::loadSphere(std::istringstream &iss)
+{
+	float r, o_x, o_y, o_z, reflect, amb_x, amb_y, amb_z, diff_x, diff_y, diff_z, spec_x, spec_y, spec_z, shininess;
+	iss >> r >> o_x >> o_y >> o_z >> reflect >> amb_x >> amb_y >> amb_z >> diff_x >> diff_y >> diff_z >> spec_x >> spec_y >> spec_z >> shininess;
+	CSphere *sphere = new CSphere;
+	sphere->r = r;
+	sphere->o = glm::vec3(o_x, o_y, o_z);
+	sphere->reflect = reflect;
+	sphere->ambient = glm::vec3(amb_x, amb_y, amb_z);
+	sphere->diffuse = glm::vec3(diff_x, diff_y, diff_z);
+	sphere->specular = glm::vec3(spec_x, spec_y, spec_z);
+	sphere->shininess = shininess;
+	this->mObjects.push_back(sphere);
 }
--- Ray Tracer/Scene.h
+++ Ray Tracer/Scene.h
@@ -12,6 +12,15 @@ public:
 	CScene(void);
 	~CScene(void);
 
-	bool parse(std::string fname); //zaimplementowac scene z pliku
+	bool parse(std::string fname);
+
+private:
+	void loadCamWidth(std::istringstream & iss);
+	void loadCamHeight(std::istringstream & iss);
+	void loadCamFOV(std::istringstream & iss);
+	void loadCamPos(std::istringstream & iss);
+	void loadCamTarget(std::istringstream & iss);
+	void loadCamUp(std::istringstream & iss);
+	void loadSphere(std::istringstream &iss);
 };
 
